23bc1912b43a7c6d5efa45ba27a60f1601c993d9
zhanglimingisagoodboy <18435179635@163.com>
2017-05-02 11:06:28 +0800

    table

--- index.js
+++ index.js
@@ -1,67 +1,57 @@
 var express=require("express");
-var ejs=require("ejs");
-var mysql=require("mysql");
 var path=require("path");
+var mysql=require("mysql");
+var parser=require("body-parser");
+var ejs=require("ejs");
 var app=express();
-
-var connnect=mysql.createConnection({
-  host:"localhost",
-  port:"3306",
-  user:"root",
-  password:"root",
-  database:"uek1610"
+app.listen(8899);
+app.set('views',path.join(__dirname,'/template'));
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'/static')));
+app.use(parser.urlencoded({ extended: true }));
+var connect=mysql.createConnection({
+    host     : 'localhost',
+    port     : '3306',
+    user     : 'root',
+    password : '',
+    database : 'gg'
+});
+app.get("/",function (req,res) {
+        res.render("index");
 })
-/*静态文件*/
- app.use(express.static(path.join(__dirname,"static")))
-
-/*模板目录以及模板引擎*/
-
-app.set("views",path.join(__dirname,"tpl"));
-app.set("view engine","ejs");
-
-
-/*指定路由*/
-
-app.get("/",function(req,res){
-    res.render("index",{name:"aa"})
-})
-app.get("/select",function(req,res){
-    connnect.query("select * from stuinfo",function(error,result){
-
-        res.send(JSON.stringify(result));
+app.get("/select",function (req,res) {
+    connect.query("select * from students",function (error,result) {
+        res.send(result);
     })
 })
-
-app.get("/update",function(req,res){
-
+app.get("/update",function (req,res) {
     var id=req.query.id;
     var val=req.query.val;
     var ziduan=req.query.ziduan;
-
-
-
-    connnect.query(`update stuinfo set ${ziduan}='${val}' where id=${id}`,function(error,result){
-      res.send("ok");
-    })
-
-
-})
-
-app.get("/del",function(req,res){
-    var id=req.query.id;
-
-    connnect.query("delete from stuinfo where id="+id,function(error,result){
+    connect.query(`update students set ${ziduan}='${val}' where id=${id}`,function (error,result) {
         if(result.affectedRows>0){
-
             res.send("ok");
         }
     })
 })
-
-app.get("/add",function(req,res){
-      connnect.query("insert into stuinfo (name,sex,age,classes) values ('','','','')",function(error,result){
-            res.send(result.insertId.toString());
-      })
+app.get("/del",function (req,res) {
+    var id=req.query.id;
+    connect.query(`delete from students where id=${id}`,function (error,result) {
+        if(result.affectedRows>0){
+            res.send("ok");
+        }
+    })
 })
-
-app.listen(8888);
+app.get("/addtable",function (req,res) {
+    var name=req.query.names;
+    var sex=req.query.sex;
+    var age=req.query.age;
+    var classes=req.query.classes;
+    console.log(name,sex,age,classes);
+    var sql=`insert into students (name,sex,age,classes) values('${name}','${sex}',${age},${classes})`;
+    connect.query(sql,function (error,result) {
+        if(result.affectedRows>0){
+            res.send("yes");
+        }
+    })
+})
--- static/js/index.js
+++ static/js/index.js
@@ -1,54 +1,60 @@
-angular.module("myapp",["service"])
-.controller("stu",["$scope","$http","$timeout","one","two",function($scope,$http,$timeout,one,two){
-
-   two();
-
-        $scope.isshow=false;
-
-        $http({url:"/select"}).then(function(data){
-          $scope.data=data.data
-        });
-
-
-        $scope.blur=function(id,val,ziduan){
-            $scope.isshow=true;
-            $http({url:"/update",method:"get",params:{id:id,val:val,ziduan:ziduan}}).then(function(data){
-                if(data.data=="ok"){
-                    $timeout(function(){
-                        $scope.isshow=false;
-                    },2000)
-                }
-            });
-        }
-
-        $scope.del=function(id){
-            $scope.isshow=true;
-           $http({url:"/del",params:{id:id}}).then(function(e){
-                if(e.data=="ok"){
-                    $scope.isshow=false;
-                    $scope.data.forEach(function(obj,index){
+angular.module("myapp",["ngRoute","ngAnimate"])
+.config(function($routeProvider){
+    $routeProvider.when("/",{
+        templateUrl:"table.ejs",
+        controller:"table"
+    }).when("/add",{
+        templateUrl:"add.ejs",
+        controller:"add"
+    })
+})
+.controller("table",["$scope","$http",function ($scope,$http) {
+    //启动时获取数据库数据
+    $http({url:"/select"}).then(function (data) {
+        $scope.data=data.data;
+    })
+//修改用户数据
+    $scope.tableblur=function (id,val,ziduan) {
+        $http({url:"/update",params:{id:id,val:val,ziduan:ziduan}}).then(function (data) {
+            if(data.data=="ok"){
+                console.log("ok");
+            }
+        })
+    }
+//删除其中一条数据
+    $scope.tabledel=function (id) {
+        $http({url:"/del",params:{id:id}}).then(function (data) {
+            if(data.data=="ok"){
+                $scope.data.forEach(function (obj,index) {
                     if(obj.id==id){
                         $scope.data.splice(index,1);
                     }
-                    })
-                }
-            })
-        }
-        
-        $scope.add=function () {
-             $http({url:"/add"}).then(function(e){
-                 if(e.data){
-                     var obj={};
-                     obj.id=e.data;
-                     obj.name="";
-                     obj.sex="";
-                     obj.classes="";
-                     obj.age="";
-                     $scope.data.push(obj);
-                 }
-
-             })
-        }
-
-
+                })
+                alert("删除成功");
+            }
+        })
+    }
+    
+}]).controller("add",["$scope","$http",function ($scope,$http) {
+    $scope.name="张三";
+    $scope.sex="男";
+    $scope.age="22";
+    $scope.classes="1610";
+    $scope.addtable=function (name,sex,age,classes) {
+        console.log(name,sex,age,classes);
+        $http({
+            method:"get",
+            url:"/addtable",
+            params:{
+                names:name,
+                sex:sex,
+                age:age,
+                classes:classes
+            }
+        }).then(function(data) {
+            if(data.data=="yes"){
+                window.location.href="#!/"
+            }
+        })
+    }
 }])
